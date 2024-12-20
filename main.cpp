584f757a953a76f0c3c6e33cee3bbd04a4a705d5
super <super@SUPERCOM_I5-WIN>
2024-12-20 21:49:32 +0200

    draw() and update() test

--- main.cpp
+++ main.cpp
@@ -6,15 +6,32 @@ void draw()
 {
 	graphics::Brush br;
 
+	graphics::setOrientation(45);
 
-	
+	br.texture = "assets\\boy2.png";
+	br.outline_opacity = 0.0f;
 
+	graphics::drawRect(500, 250, 200, 200, br);
+
+	br.fill_color[0] = 0.5f;
+	br.fill_color[1] = 0.0f;
+	br.fill_color[2] = 0.0f;
+
+	graphics::resetPose();
+	drawText(300, 440, 70, "Hello World!", br);
 
 	
 }
 
 void update(float dt)
 {
+
+	graphics::MouseState mouse;
+	graphics::getMouseState(mouse);
+	if (mouse.button_left_released)
+	{
+		graphics::playSound("assets\\door2.wav", 1.0f, false);
+	}
 
 }
 
